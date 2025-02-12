1eecfec3bc1d2f67089f8585823718e37b0cba37
Vanessa Silva <vanessa.tecma@gmail.com>
2025-02-12 12:50:48 -0300

    ultimo_exercicio_SEmana1

---
+++ soma.py
@@ -0,0 +1,13 @@
+
+# Desenvolver os diagramas de blocos e as codificações em português estruturado
+# usando laço incondicional (para) do seguinte problema: Construir um programa
+# que apresente a soma dos cem primeiros números
+# naturais (1 + 2 + 3 +...+ 98 + 99 + 100).
+
+Inicio
+    soma ← 0
+    Para i de 1 até 100 faça
+        soma ← soma + i
+    FimPara
+    Escreva("A soma dos números de 1 a 100 é: ", soma)
+Fim
